624b8c1695e116717018fac44d3302767267f434
sardorxonakramov <sardorxonakramov01@gmail.com>
2025-04-06 03:05:41 +0500

    Login va Logout sozlandi

--- CONFIG/settings.py
+++ CONFIG/settings.py
@@ -123,4 +123,8 @@ MEDIA_ROOT = BASE_DIR/'media'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
 AUTH_USER_MODEL = 'USERS.CustomUser'
+
+LOGOUT_REDIRECT_URL = 'home'
+LOGOUT_REDIRECT_URL = 'login'
--- USERS/views.py
+++ USERS/views.py
@@ -3,7 +3,7 @@ from django.urls import reverse_lazy
 from django.contrib import messages
 from django.views.generic import CreateView
 from django.contrib.auth import get_user_model
-from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth.views import LoginView
 
 from .forms import CustomUserCreationForm
 # Create your views here.
@@ -13,13 +13,25 @@ class RegisterView(CreateView):
     model = User
     template_name = 'USERS/register.html'
     form_class = CustomUserCreationForm
-    success_url = reverse_lazy('home')
+    success_url = reverse_lazy('login')
 
     def form_valid(self, form):
         response = super().form_valid(form)
-        messages.success(self.request, "User created successfully")
+        messages.success(self.request, "Akkountingiz yaratildi!")
         return response
 
     def form_invalid(self, form):
-        messages.error(self.request, "User creation failed")
+        messages.error(self.request, "Akkount yaratishda xatolik!")
         return super().form_invalid(form)
+
+class CustomLoginView(LoginView):
+    template_name = 'USERS/login.html'
+    def get_success_url(self):
+        return reverse_lazy('home')
+    
+    def form_valid(self, form):
+        response = super().form_valid(form)
+        messages.success(self.request, "Tizimga Muvaffaqiyatli kirdingiz!")
+        return response
+
+
